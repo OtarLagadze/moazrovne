52c242e2a25ab8f796b971b1f824378c7cda79f4
OtarLagadze <otikolagadze.23@gmail.com>
2025-04-28 23:29:03 +0400

    old landingImage

--- components/ui/LandingImage.jsx
+++ components/ui/LandingImage.jsx
@@ -4,10 +4,9 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import classes from "./LandingImage.module.css";
 
-import welcome1 from "@/public/welcome-photos/welcome1.jpeg";
-import welcome2 from "@/public/welcome-photos/welcome2.jpeg";
-import welcome3 from "@/public/welcome-photos/welcome3.jpeg";
-import welcome4 from "@/public/welcome-photos/welcome4.jpeg";
+import welcome1 from "@/public/welcome-photos/welcome-1.webp";
+import welcome2 from "@/public/welcome-photos/welcome-2.webp";
+import welcome3 from "@/public/welcome-photos/welcome-3.webp";
 
 export default function LandingImage() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -17,8 +16,7 @@ export default function LandingImage() {
       setCurrentIndex((prev) => {
         if (prev === 0) return 1;
         if (prev === 1) return 2;
-        if (prev === 2) return 3;
-        if (prev === 3) return 0;
+        if (prev === 2) return 0;
       });
     }, 5000);
 
@@ -32,7 +30,7 @@ export default function LandingImage() {
           currentIndex === 0 ? classes.show : classes.hide
         }`}
         src={welcome1}
-        alt="Photo of Rome"
+        alt="School children, who are happy in class room and are writing test"
         priority={true}
       />
       <Image
@@ -40,21 +38,14 @@ export default function LandingImage() {
           currentIndex === 1 ? classes.show : classes.hide
         }`}
         src={welcome2}
-        alt="Photo of Rome"
+        alt="Student who is writing homework"
       />
       <Image
         className={`${classes.heroImage} ${
           currentIndex === 2 ? classes.show : classes.hide
         }`}
         src={welcome3}
-        alt="Photo of Rome"
-      />
-      <Image
-        className={`${classes.heroImage} ${
-          currentIndex === 3 ? classes.show : classes.hide
-        }`}
-        src={welcome4}
-        alt="Photo of Rome"
+        alt="Students are happy and are writing homework"
       />
     </>
   );
